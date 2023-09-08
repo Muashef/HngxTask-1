c2dabca602c3350083f324e640009e54da6529ca
Sherif <muhammedsherif40@yahoo.com>
2023-09-08 10:01:40 +0100

    fixes

--- script.js
+++ script.js
@@ -2,34 +2,30 @@ const background = document.querySelector('.background');
 const container = document.querySelector('.container');
 
 function displayDateTime() {
-  const currentDate = new Date();
+    // Get the current date and time
+    const currentDate = new Date();
+  
+    const dayOfWeek = currentDate.toLocaleDateString('en-US', {
+      weekday: 'long',
+    });
+  
+    const formattedDayOfWeek = `${dayOfWeek}`;
+  
+    const dayOfWeekDiv = document.getElementById('dayOfWeek');
+  
+    dayOfWeekDiv.textContent = formattedDayOfWeek;
+  
+    const currentTimeInMilliseconds = Date.now();
+  
+    const currentTimeDiv = document.getElementById('currentTimeInMilliseconds');
+  
+    currentTimeDiv.textContent = `Current UTC Time: ${currentTimeInMilliseconds}`;
+  }
+  
+  // Update the displayed date and time every second
+  setInterval(displayDateTime, 1000);
+  
+  displayDateTime();
 
-  const dayOfWeek = currentDate.toLocaleDateString('en-US', {
-    weekday: 'long',
-  });
 
-  const formattedDayOfWeek = `${dayOfWeek}`;
 
-  const dayOfWeekDiv = document.getElementById('dayOfWeek');
-
-  dayOfWeekDiv.textContent = formattedDayOfWeek;
-
-  const options = {
-    timeZone: 'UTC',
-    hour12: false,
-    hour: '2-digit',
-    minute: '2-digit',
-    second: '2-digit',
-  };
-
-  const formattedTime = currentDate.toLocaleTimeString('en-US', options);
-
-  const currentTimeDiv = document.getElementById('currentTimeInMilliseconds');
-
-  currentTimeDiv.textContent = `Current UTC Time: ${formattedTime}`;
-}
-
-setInterval(displayDateTime, 1000);
-
-// Initial call to displayDateTime function
-displayDateTime();
